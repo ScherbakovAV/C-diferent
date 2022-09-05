7b910739b80193fad9bdf6853d7c3ad58fa76af6
Scherbakov Anatoly <axidarr@gmail.com>
2022-09-05 19:59:48 +0300

    Added pyramid

---
+++ Pyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿void DrawPyramid(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            Console.Write("  ");        
+        }
+
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            Console.Write("*" + " ");        
+        }
+        Console.WriteLine();
+    }
+}
+
+DrawPyramid(50);
